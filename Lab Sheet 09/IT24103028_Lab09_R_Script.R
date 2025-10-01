40b4ce7f9f286173089c30be45b3e5ecd3730fd0
IT24103028 <it24103028@my.sliit.lk>
2025-10-01 17:19:48 +0530

    IT24103028_Lab09

---
+++ Lab Sheet 09/IT24103028_Lab09_R_Script.R
@@ -0,0 +1,10 @@
+setwd("D:\\USER\\Desktop\\SLITT\\Y2S1\\PS ( IT2120)\\Labs ( PS )\\IT24103028_Lab09_PS")
+getwd()
+
+
+#01
+y <- rnorm(25, mean = 45, sd = 2)
+
+
+#02
+t.test(y, mu =46 , alternative = "less")
